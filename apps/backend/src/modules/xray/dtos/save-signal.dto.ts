a911f74d4d9b01f6c49d9bbbe5cd64e0939a4679
aref-hammaslak <arefhammaslak@gmail.com>
2025-08-14 15:51:38 +0330

    feat(xray): enhance SaveSignalDto with Swagger documentation
    - Added ApiProperty decorators to SaveSignalDto for better API documentation.
    - Included descriptions and examples for deviceId, time, and data properties.
    - Improved clarity and usability of the DTO in the context of API interactions.

--- apps/backend/src/modules/xray/dtos/save-signal.dto.ts
+++ apps/backend/src/modules/xray/dtos/save-signal.dto.ts
@@ -6,16 +6,32 @@ import {
   IsString,
   Validate,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SaveSignalDto {
+  @ApiProperty({
+    description: 'The device ID in UUID format',
+    example: '66bb584d4ae73e488c30a072',
+  })
   @IsNotEmpty()
   @IsString()
   deviceId: string;
 
+  @ApiProperty({
+    description: 'The time (milliseconds since epoch)',
+    example: 1735683480000,
+  })
   @IsNotEmpty()
   @IsNumber()
   time: number;
 
+  @ApiProperty({
+    description: 'The data (array of [time, [speed, latitude, longitude]])',
+    example: [
+      [762, [51.33, 12.33, 1.2]],
+      [1766, [51.33, 12.33, 1.53]],
+    ],
+  })
   @IsNotEmpty()
   @IsArray()
   @ArrayMinSize(1)
