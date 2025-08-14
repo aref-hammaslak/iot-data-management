99a11874277535bd28524b9a9bc47b05434e003c
aref-hammaslak <arefhammaslak@gmail.com>
2025-08-14 11:06:27 +0330

    feat(xray): implement query and deletion methods in SignalService
    - Added methods for finding and deleting signals based on SignalQueryFilterDto.
    - Implemented buildQuery method to construct dynamic queries for filtering signals.

--- apps/backend/src/modules/xray/signal.service.ts
+++ apps/backend/src/modules/xray/signal.service.ts
@@ -1,10 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
 import { XRaySignal } from '../database/models/xray-signal.model';
 import { SaveSignalDto } from './dtos/save-signal.dto';
-import { validate } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
+import { SignalQueryFilterDto } from './dtos/signal-query-filter.dto';
+import { validate } from 'class-validator';
 
 @Injectable()
 export class SignalService {
@@ -29,5 +30,58 @@ export class SignalService {
       rawData: data,
     });
     await xraySignal.save();
+  }
+
+  async findAll(signalFilter: SignalQueryFilterDto) {
+    const query = this.buildQuery(signalFilter);
+    return this.xraySignalModel.find(query).exec();
+  }
+
+  async deleteAll(signalFilter: SignalQueryFilterDto) {
+    const query = this.buildQuery(signalFilter);
+    return this.xraySignalModel.deleteMany(query).exec();
+  }
+
+  async findOne(id: string) {
+    return this.xraySignalModel.findById(id).exec();
+  }
+
+  async deleteOne(id: string) {
+    return this.xraySignalModel.findByIdAndDelete(id).exec();
+  }
+
+  buildQuery(signalFilter: SignalQueryFilterDto): FilterQuery<XRaySignal> {
+    const {
+      deviceId,
+      timeStart,
+      timeEnd,
+      dataVolumeMin,
+      dataVolumeMax,
+      dataLengthMin,
+      dataLengthMax,
+    } = signalFilter;
+    const query: FilterQuery<XRaySignal> = {};
+    if (deviceId) {
+      query.deviceId = deviceId;
+    }
+    if (timeStart) {
+      query.time = { $gte: timeStart };
+    }
+    if (timeEnd) {
+      query.time = { $lte: timeEnd };
+    }
+    if (dataVolumeMin) {
+      query.dataVolume = { $gte: dataVolumeMin };
+    }
+    if (dataVolumeMax) {
+      query.dataVolume = { $lte: dataVolumeMax };
+    }
+    if (dataLengthMin) {
+      query.dataLength = { $gte: dataLengthMin };
+    }
+    if (dataLengthMax) {
+      query.dataLength = { $lte: dataLengthMax };
+    }
+    return query;
   }
 }
