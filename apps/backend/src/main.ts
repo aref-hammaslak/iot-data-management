d1abc4eac85afc22b2833999a2118636464c3c99
aref-hammaslak <arefhammaslak@gmail.com>
2025-08-14 18:51:53 +0330

    feat(backend): add Swagger configuration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { BackendModule } from './backend.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(BackendModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('IOT Data Management API')
+    .setDescription('API for x-ray data management')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.port ?? 3000);
+  logger.log(`Server is running on port ${process.env.port ?? 3000}`);
 }
 bootstrap();
